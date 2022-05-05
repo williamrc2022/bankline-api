4cb15aa6685885a1b3f2776a4154be8ffbf84934
William Costa <williamrc2022@gmail.com>
2022-05-05 00:37:00 -0300

    adicionando classes a aplicacao

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,5 +1,20 @@
 package com.dio.santander.bankline.api.model;
 
 public class Conta {
+	private Long numero;
+	private double saldo;
+	public 
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 
 }
